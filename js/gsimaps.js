47d775c5c1fa8d8d4dd6dd828e06979b98b4f8b1
tanphat132 <97350150+tanphat132@users.noreply.github.com>
2022-07-09 19:46:19 +0700

    Add files via upload

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -13577,7 +13577,7 @@ GSI.GeoJSON = L.Layer.extend({
     geoJSONOptions.onEachFeature = L.bind(this.onEachFeature, this);
     geoJSONOptions.pointToLayer = L.bind(this.onPointToLayer, this);
     geoJSONOptions.geodesic = this.options.geodesic;
-    this.layer = new L.GeoJSON(null, geoJSONOptions);
+    this.layer = new L.geoJson.css(null, geoJSONOptions);
 
     if (!this._layerAdded && this._map) {
       var zoom = this._map.getZoom();
@@ -53016,3 +53016,120 @@ function getFileeData(url, key) {
     });
   }
 };
+
+(function (window, document, undefined) {
+	if ( !window.L || !L.GeoJSON ) {
+		return;
+	}
+
+	function setPopPup(feature, json) {
+		
+		feature.on('click', function (e) {				
+			feature.bindPopup(getContent(json));		
+			this.openPopup();
+		});
+
+		if ( !(feature instanceof L.Marker) ) {
+			feature.on('mouseover', function (e) {
+				var highlight = {
+					color: '#2262CC',
+		        	weight: 3,		                   
+		        	fillColor: '#2262CC'
+				};
+
+				feature.setStyle(highlight);			           
+			});
+		}
+
+	}
+
+	function getContent(feature) {
+		var popupContent = '<table>';
+		for (var p in feature.properties) {
+			popupContent += '<tr><td><b>' + p + ':</b></td><td>'+ feature.properties[p] + '</td></tr>';
+		}
+		
+		popupContent += '</table>';
+		return 	popupContent;			
+	}
+
+
+	function getLayerMarkerStyle(style, latlng) {
+		if ( style.icon ) {
+			var smallIcon = new L.Icon({
+				iconSize: style.icon.iconSize,
+     			iconAnchor: style.icon.iconAnchor,
+     			popupAnchor:  style.icon.popupAnchor,							    
+				iconUrl:  style.icon.iconUrl
+			});
+
+			return new L.Marker(latlng,{icon: smallIcon});
+		}
+
+		return new L.CircleMarker(latlng, style);	
+	}
+
+	function setStyle(feature, style) {
+		if ( !(feature instanceof L.Marker) ) {                    		
+			feature.setStyle(style);
+
+			feature.on('mouseout', function (e) {							
+				           feature.setStyle(style);
+				        });							
+		}
+	}
+
+	L.GeoJSON.CSS = L.GeoJSON.extend({
+		initialize: function (geojson, options) {
+
+			var layerStyle = geojson.style;
+
+
+			var styledOptions = L.extend({}, options, {
+
+				 pointToLayer: function (feature, latlng) {   
+				 	var featureStyle = feature.style;
+
+                    if ( featureStyle ) {							
+						return getLayerMarkerStyle(featureStyle, latlng);													
+		            }		            
+		            else if(layerStyle) {
+		            	return  getLayerMarkerStyle(layerStyle, latlng);		            	
+		        	}
+		        	else {
+		        		//default point layer
+		        		return new L.Marker(latlng, {});
+		        	}
+		            
+		          },
+
+				onEachFeature: function(json, feature) {
+   										
+                    if(json.style) {                    	
+                    	setStyle(feature, json.style);																		
+					}
+					else if(layerStyle) {							
+						setStyle(feature, layerStyle);				
+					}		
+
+					setPopPup(feature, json);
+					
+				}
+			});
+
+			
+			L.setOptions(this, styledOptions);
+
+			this._layers = {};
+
+			if (geojson) {
+				this.addData(geojson);				
+			}
+		}
+});
+
+
+L.geoJson.css = function (geojson, options) {
+		return new L.GeoJSON.CSS(geojson, options);
+	};
+})(window, document);
